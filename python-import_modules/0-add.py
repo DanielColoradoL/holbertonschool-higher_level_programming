16a1be43782857a0b82652941881d2c2d23fd7ec
Daniel Colorado <danielcoloradol@hotmail.com>
2024-01-27 16:28:31 +1100

    1 fixed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-
-def add(a, b):
-    if __name__ == "__main__":
-        print("{0} + {1} = {2}".format(a, b, a + b))
-        return (a + b)
-
-
-add(a, b)
+from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a + b)))
