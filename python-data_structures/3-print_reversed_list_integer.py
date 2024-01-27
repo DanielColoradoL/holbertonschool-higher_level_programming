8f1da83cb0280a2ab9f8755496cfc22d4603221f
Daniel Colorado <danielcoloradol@hotmail.com>
2024-01-27 19:22:20 +1100

    0 and 3 second

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     start = len(my_list) - 1
     for i in range(start, -1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
