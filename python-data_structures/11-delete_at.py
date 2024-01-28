99346c42f4f32d41d288b7182af47c174d5c77ab
Daniel Colorado <danielcoloradol@hotmail.com>
2024-01-28 18:29:08 +1100

    10 .. 12 second

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     leng = len(my_list)
-    if leng == 0 or idx >= leng:
+    if leng == 0 or idx >= leng or idx < 0:
         return (my_list)
     key = my_list[idx]
     my_list.remove(key)
