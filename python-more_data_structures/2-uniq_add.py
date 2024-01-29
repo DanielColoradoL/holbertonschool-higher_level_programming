e4f2095316f776a4f280e7b01a3d4d6e7a9d05b7
Daniel Colorado <danielcoloradol@hotmail.com>
2024-01-29 14:53:40 +1100

    2 second

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     output = 0
+    tmp = list()
     for num in my_list:
-        if my_list.count(num) == 1:
-            output = output + num
+        if tmp.count(num) == 0:
+            tmp.append(num)
+    for num in tmp:
+        output = output + num
     return output
