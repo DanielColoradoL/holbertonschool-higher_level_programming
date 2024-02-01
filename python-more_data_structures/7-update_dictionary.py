4dd135360a68e280a5356240c83ec027c6401b93
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-01 15:56:12 +1100

    7 fix

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary.keys():
-        print(f"This is key {key} and this is value {value}")
         a_dictionary[key] = value
     else:
         a_dictionary.update({key: value})
