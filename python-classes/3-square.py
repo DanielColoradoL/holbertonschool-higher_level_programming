27254ab998fcabb46b88779889c992132ff23fc3
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-03 16:58:31 +1100

    2 rollback, 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,3 +17,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square
+
+        Return:
+            The area of the square.
+        """
+        return (self.__size ** 2)
