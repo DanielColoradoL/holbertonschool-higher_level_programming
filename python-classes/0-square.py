6ce63dec7a9d16b89f010084bc56a7f6b5eaf24a
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-03 16:30:25 +1100

    0 docstring update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
+""" Defines a square """
+
+
 class Square:
-    """
-    Square - Defines a square
-
-    Notes:
-        handles div/0 error, index error and type error
-
-    Args:
-        my_list_1: The dividend list
-        my_list_2: The divisor list
-
-    Return:
-        utput_list: the resulting list of the division
-    """
+    """ Represent a square """
     pass
