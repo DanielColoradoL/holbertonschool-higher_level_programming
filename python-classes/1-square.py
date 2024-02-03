9a341a8d22005e0dea2f119d01a1920ae297ccd4
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-03 16:36:56 +1100

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Defines a square """
+
+
+class Square:
+    """ Represent a square """
+    def __init__(self, size):
+        """
+        initializing new objects
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
