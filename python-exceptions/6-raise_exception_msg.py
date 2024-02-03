de5b1f5b0f544c91ac5d19671f5f01cea7f45df3
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-03 13:02:34 +1100

    6

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    raise_exception - raise an exception
+    raise a NameError with a message
+    """
+    raise NameError(message)
