ee654eae3011d1516769569484893652a0c4e437
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-03 16:50:58 +1100

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Defines a square """
+
+
+class Square:
+    """ Represent a square """
+    def __init__(self, size=0):
+        """
+        initializing new objects
+        Size must be an integer and > 0
+        Else it will raise an error
+        Args:
+            size: The size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
