0b9687a3cdfa4fd3fb6f20e556868b7230544608
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-06 16:12:11 +1100

    5 w test

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module containing say_my_name function only
+"""
+
+
+def text_indentation(text):
+    """
+    text_indentation - Print the input text
+        split by '.', '?', ':'
+        add a new line after one
+        of the previous chars are found
+        It checks that text is a string
+
+    args:
+        text: the string to split
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    start = 0
+    for index, char in enumerate(text):
+        if char in ['.', '?', ':']:
+            if index < len(text):
+                index += 1
+            print(text[start: index])
+            print("")
+            if text[index] == " ":
+                index += 1
+            start = index
+    print(text[start: index + 1])
