cd8e95e5f028c6adba530c4bbf2912f75ce9d089
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-09 16:12:56 +1100

    7 no tests

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module containing BaseGeometry class  """
+
+
+class BaseGeometry:
+    """ Represent a BaseGeometry """
+
+    def area(self):
+        """ area raise an exception as
+        it has not been implemented yet """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates an integer
+
+        args:
+            name = string representing a name
+            value = value to validate """
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
