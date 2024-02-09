2378aa20c14ecf2b5c539316acc9cdbb45e084a9
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-09 17:31:08 +1100

    11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Represent a Rectangle
+    Inherits from BaseGeometry """
+
+    def __init__(self, size):
+        """ Creates a new instance of Square
+        It validates size
+        using integer_validator
+        inherited from Rectangle --> BaseGeometry
+
+        args:
+            size = square size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculates the area"""
+        return self.__size ** 2
