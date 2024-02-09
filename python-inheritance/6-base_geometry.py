681909586551511a92be59f184a93c4c505550d1
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-09 16:06:33 +1100

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module containing BaseGeometry class  """
+
+
+class BaseGeometry:
+    """ Represent a BaseGeometry """
+
+    def area(self):
+        """ area raise an exception as
+        it has not been implemented yet """
+        raise Exception("area() is not implemented")
