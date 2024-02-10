887a26c7f268037a4140c98abf0b68f64873b5c4
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-10 12:17:19 +1100

    8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module containing class_to_json function"""
+
+
+def class_to_json(obj):
+    """ returns the dictionary description
+    with simple data structure"""
+    return obj.__dict__
