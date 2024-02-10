ae27ccfb5de69872930a98f6b6b2dc988213bab3
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-10 12:38:43 +1100

    10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module containing Student Class"""
+
+
+class Student():
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with basic attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns a dictionary with simple data structure
+        If no attrs are given, returns all key, value pairs
+        If attrs are given, It only returns a dictionary
+        with the given keys in it"""
+        if attrs is None:
+            return self.__dict__
+
+        output_dic = {}
+        for key in self.__dict__:
+            if key in attrs:
+                output_dic[key] = self.__dict__.get(key)
+        return output_dic
