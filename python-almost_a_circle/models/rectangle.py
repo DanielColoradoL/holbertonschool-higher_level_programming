2a6cdcfa748471b29f87cf1236cfcda419008981
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-13 11:54:39 +1100

    3 validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,26 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(id)
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         self.__width = width
         self.__height = height
         self.__x = x
@@ -22,6 +42,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """Modify the private width attribute"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = width
 
     @property
@@ -32,6 +57,11 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """Modify the private height attribute"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = height
 
     @property
@@ -42,6 +72,11 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """Modify the private x attribute"""
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = x
 
     @property
@@ -52,4 +87,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """Modify the private y attribute"""
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = y
