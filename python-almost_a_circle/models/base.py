29167028700cec5d6fa2985d46cfe26d7fea07df
Daniel Colorado L <danielcoloradol@hotmail.com>
2024-02-14 13:03:18 +1100

    15 dic to Json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module contains the Base class"""
+import json
 
 
 class Base():
@@ -16,3 +17,12 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string from
+        list_dictionaries"""
+        leng = len(list_dictionaries)
+        if list_dictionaries is None or leng == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
