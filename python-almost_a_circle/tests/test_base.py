cb2acb08ed48bd879b7025d4705568523ca191dd
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-17 12:46:30 +1100

    tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """Unittests for the class Base"""
 import unittest
-import os
 from models.base import Base
-
 
 
 class TestBase_instantiation(unittest.TestCase):
     """Define unittests for Base_Test([..])."""
 
-    def test_base(self):
-        """Test the class base
-        basic features"""
+    def test_id(self):
+        """Test instantiation id"""
         b1 = Base()
         b2 = Base()
         b3 = Base(89)
@@ -20,34 +17,28 @@ class TestBase_instantiation(unittest.TestCase):
         self.assertEqual(b2.id, 2)
         self.assertEqual(b3.id, 89)
 
+    def test_to_json_string(self):
+        """Test the json to string function"""
         a = Base.to_json_string(None)
         self.assertEqual(a, "[]")
-        """ "[]" """
 
         b = Base.to_json_string([])
         self.assertEqual(b, "[]")
-        """ "[]" """
 
-        c = Base.to_json_string([{'id': 12 }])
+        c = Base.to_json_string([{'id': 12}])
         self.assertEqual(c, '[{"id": 12}]')
-        """ "[{"id": 12}]" """
 
-        d = type(Base.to_json_string([ {'id': 12 }]))
+        d = type(Base.to_json_string([{'id': 12}]))
         self.assertEqual(d, str)
-        """ "<class 'str'>" """
 
         e = Base.from_json_string(None)
         self.assertEqual(e, [])
-        """ [] este es una type list """
 
         f = Base.from_json_string("[]")
         self.assertEqual(f, [])
-        """ [] este es una type list """
 
         g = Base.from_json_string('[{ "id": 89 }]')
         self.assertEqual(g, [{'id': 89}])
-        """ [{'id': 89}] """
 
         h = type(Base.from_json_string('[{ "id": 89 }]'))
         self.assertEqual(h, list)
-        """ "<class 'list'>" """
