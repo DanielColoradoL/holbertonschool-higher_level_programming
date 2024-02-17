f65458efd4ae1126b1fd054428dbf7224c179d66
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-17 13:55:59 +1100

    Rectangle tests added

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -7,47 +7,63 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """Define unittests for TestRectangle"""
 
-    def test_instantiation(self):
-        """Test instantiation id"""
-
+    def test_widht(self):
+        """Test width attribute"""
         a = Rectangle(1, 2)
-        self.assertEqual(str(a), "[Rectangle] (1) 0/0 - 1/2")
+        self.assertEqual(a.width, 1)
+        b = Rectangle(15, 2, 3)
+        self.assertEqual(b.width, 15)
+        c = Rectangle(3, 2, 3, 4)
+        self.assertEqual(c.width, 3)
+        d = Rectangle(4, 2, 3, 4, 5)
+        self.assertEqual(d.width, 4)
 
-        b = Rectangle(1, 2, 3)
-        self.assertEqual(str(b), "[Rectangle] (2) 3/0 - 1/2")
+    def test_height(self):
+        """Test height attribute"""
+        a = Rectangle(1, 2)
+        self.assertEqual(a.height, 2)
+        b = Rectangle(15, 2, 3)
+        self.assertEqual(b.height, 2)
+        c = Rectangle(3, 6, 3, 4)
+        self.assertEqual(c.height, 6)
+        d = Rectangle(4, 44, 3, 4, 5)
+        self.assertEqual(d.height, 44)
 
-        c = Rectangle(1, 2, 3, 4)
-        self.assertEqual(str(c), "[Rectangle] (3) 3/4 - 1/2")
-
-        h = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(str(h), "[Rectangle] (5) 3/4 - 1/2")
-
+    def test_instantiation_errors(self):
+        """Test instantiation errors"""
         with self.assertRaises(TypeError):
             Rectangle("1", 2)
-
         with self.assertRaises(TypeError):
             Rectangle(1, "2")
-
         with self.assertRaises(TypeError):
             Rectangle(1, 2, "3")
-
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 3, "4")
-
         with self.assertRaises(ValueError):
             Rectangle(-1, 2)
-
         with self.assertRaises(ValueError):
             Rectangle(1, -2)
-
         with self.assertRaises(ValueError):
             Rectangle(0, 2)
-
         with self.assertRaises(ValueError):
             Rectangle(1, 0)
-
         with self.assertRaises(ValueError):
             Rectangle(1, 2, -3)
-
         with self.assertRaises(ValueError):
             Rectangle(1, 2, 3, -4)
+
+    def test_print(self):
+        """Test the default print of the class"""
+        a = Rectangle(1, 1, 1, 1, 1)
+        self.assertEqual(str(a), "[Rectangle] (1) 1/1 - 1/1")
+        b = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(str(b), "[Rectangle] (5) 3/4 - 1/2")
+
+    def test_area(self):
+        """Test the area funtion"""
+        a = Rectangle(2, 3)
+        self.assertEqual(a.area(), 6)
+        b = Rectangle(3, 3)
+        self.assertEqual(b.area(), 9)
+        c = Rectangle(5, 5)
+        self.assertEqual(c.area(), 25)
