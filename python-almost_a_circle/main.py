35ffb6866b2e094f14939ba267e5d60fc59eb8c4
Daniel Colorado <danielcoloradol@hotmail.com>
2024-02-17 15:21:44 +1100

    more rectangle tests

--- python-almost_a_circle/main.py
+++ python-almost_a_circle/main.py
@@ -7,47 +7,11 @@ from models.base import Base
 
 
 
-a = Rectangle(2, 3)
-print(a.area())
-# 6
+a = Rectangle(2, 2, 2, 2, 1)
+#print(a.update())
+#None
 
-
-b = Rectangle(1, 1, 1, 1, 1)
-print(b)
-# [Rectangle] (1) 1/1 - 1/1
-
-
-c = Rectangle(2, 3)
-""" c.display() """
-"""
-##
-##
-##
-"""
-
-d = Rectangle(2, 2, 3)
-d.display()
-"""
-  ##
-  ##
-"""
-
-e = Rectangle(2, 2, 2, 2)
-e.display()
-"""
-
-  ##
-  ##
-"""
-
-f = Rectangle(1, 2, 3, 4, 10)
-dic = f.to_dictionary()
-print(dic)
-# Test of to_dictionary() in Rectangle exists
-
-
-
-# Test of update() in Rectangle exists
+print(a.update(89))
 
 # Test of update(89) in Rectangle exists
 
@@ -59,6 +23,9 @@ print(dic)
 
 # Test of update(89, 1, 2, 3, 4) in Rectangle exists
 
+
+# Con **kargs
+
 # Test of update(**{ 'id': 89 }) in Rectangle exists
 
 # Test of update(**{ 'id': 89, 'width': 1 }) in Rectangle exists
@@ -68,6 +35,10 @@ print(dic)
 # Test of update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 }) in Rectangle exists
 
 # Test of update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 }) in Rectangle exists
+
+
+
+
 
 
 # Test of Rectangle.create(**{ 'id': 89 }) in Rectangle exists
--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Unittests for the class Rectangle"""
 import unittest
+import sys
+import os
+from io import StringIO
 from models.rectangle import Rectangle
 
 
@@ -67,3 +70,82 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(b.area(), 9)
         c = Rectangle(5, 5)
         self.assertEqual(c.area(), 25)
+
+    def test_display(self):
+        """Test the display function"""
+
+        """Display without x and y"""
+        a = Rectangle(2, 3)
+        a_desired = "##\n##\n##\n"
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        a.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), a_desired)
+        """Display without y"""
+        b = Rectangle(2, 2, 3)
+        b_desired = "   ##\n   ##\n"
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        b.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), b_desired)
+        """Display with x and y"""
+        c = Rectangle(2, 2, 2, 2)
+        c_desired = "\n\n  ##\n  ##\n"
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        c.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), c_desired)
+
+    def test_to_dictionary(self):
+        """Test the to dictionary function"""
+        a = Rectangle(1, 2, 3, 4, 10)
+        a_desired = {'id': 10, 'width': 1, 'height': 2, 'x': 3, 'y': 4}
+        dic = a.to_dictionary()
+        self.assertEqual(dic, a_desired)
+
+    def test_update(self):
+        """Test the update function"""
+
+        """Tests with args[]"""
+        a = Rectangle(10, 10, 10, 10, 1)
+        a.update()
+        self.assertEqual(str(a), "[Rectangle] (1) 10/10 - 10/10")
+        a.update(89)
+        self.assertEqual(str(a), "[Rectangle] (89) 10/10 - 10/10")
+        b = Rectangle(10, 10, 10, 10, 1)
+        b.update(89, 1)
+        self.assertEqual(str(b), "[Rectangle] (89) 10/10 - 1/10")
+        c = Rectangle(10, 10, 10, 10, 1)
+        c.update(89, 1, 2)
+        self.assertEqual(str(c), "[Rectangle] (89) 10/10 - 1/2")
+        d = Rectangle(10, 10, 10, 10, 1)
+        d.update(89, 1, 2, 3)
+        self.assertEqual(str(d), "[Rectangle] (89) 3/10 - 1/2")
+        e = Rectangle(10, 10, 10, 10, 1)
+        e.update(89, 1, 2, 3, 4)
+        self.assertEqual(str(e), "[Rectangle] (89) 3/4 - 1/2")
+        """Tests with **kargs{}"""
+        ka = Rectangle(10, 10, 10, 10, 25)
+        ka.update(**{'id': 89})
+        self.assertEqual(str(ka), "[Rectangle] (89) 10/10 - 10/10")
+        kb = Rectangle(10, 10, 10, 10, 25)
+        kb.update(**{'id': 89, 'width': 1})
+        self.assertEqual(str(kb), "[Rectangle] (89) 10/10 - 1/10")
+        kc = Rectangle(10, 10, 10, 10, 25)
+        kc.update(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(str(kc), "[Rectangle] (89) 10/10 - 1/2")
+        kd = Rectangle(10, 10, 10, 10, 25)
+        kd.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(str(kd), "[Rectangle] (89) 3/10 - 1/2")
+        ke = Rectangle(10, 10, 10, 10, 25)
+        ke.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(str(ke), "[Rectangle] (89) 3/4 - 1/2")
+
+    def test_create(self):
+        """Test the create static method"""
+        a = Rectangle(1, 1)
+        b = a.create(**{'id': 89, 'width': 10, 'height': 20, 'x': 3, 'y': 4})
+        self.assertNotEqual(a, b)
