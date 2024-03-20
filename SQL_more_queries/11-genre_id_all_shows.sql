27d7290d265a8d2ff9004c6d0e3a04f75e8d448a
Daniel Colorado <danielcoloradol@hotmail.com>
2024-03-20 15:30:33 +1100

    fix(task 11, 12):Updated the comments on what the query do

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- lists all shows contained in the database hbtn_0d_tvshows.
 SELECT a.title, b.genre_id 
 FROM tv_shows a LEFT JOIN tv_show_genres b
 ON b.show_id = a.id
--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,4 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT a.title, b.genre_id 
 FROM tv_shows a LEFT JOIN tv_show_genres b
 ON a.id = b.show_id
