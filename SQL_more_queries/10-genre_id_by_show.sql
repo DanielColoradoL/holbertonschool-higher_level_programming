150df10657c47ce535991988159c6924bc466d7b
Daniel Colorado <danielcoloradol@hotmail.com>
2024-03-20 15:18:47 +1100

    feat(task 10):Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT a.title, b.genre_id 
+FROM tv_shows a RIGHT JOIN tv_show_genres b
+ON b.show_id = a.id
+ORDER BY a.title, b.genre_id;
