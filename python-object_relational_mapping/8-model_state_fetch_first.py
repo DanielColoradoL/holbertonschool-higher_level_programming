8318e58742843d2cd8a07457a33e70cd94c3fb06
Daniel Colorado <danielcoloradol@hotmail.com>
2024-03-26 14:40:49 +1100

    fix(task 8):added if to print nothing if empty

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,4 +16,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).first()
-    print(f"{state.id}: {state.name}")
+    if state is None:
+        print("Nothing")
+    else:
+        print(f"{state.id}: {state.name}")
