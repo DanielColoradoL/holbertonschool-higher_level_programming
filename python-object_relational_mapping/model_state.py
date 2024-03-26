61996fc81178b2b90919c331797ea7124644ba1d
Daniel Colorado <danielcoloradol@hotmail.com>
2024-03-26 13:52:03 +1100

    feat(task 6):First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Represent the table states"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name =  Column(String(128))
