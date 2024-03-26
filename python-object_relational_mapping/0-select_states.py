cf0f7b290ae9929d239f24ad710e3930e45954ec
Daniel Colorado <danielcoloradol@hotmail.com>
2024-03-26 11:21:48 +1100

    fix(task 0):pycodestyle and docstring

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+import sys
+import MySQLdb
+
+
 """
 Get all states
 Creates a connection to a given db
 then executes a query and print the respective rows
 """
-import sys
-import MySQLdb
+try:
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states")
+    rows = cur.fetchall()
 
-db = MySQLdb.connect(host="localhost",
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3],
-                     port=3306)
-cur = db.cursor()
-cur.execute("SELECT id, name FROM states")
-rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-for row in rows:
-    print(row)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
+except MySQLdb.Error as e:
+    print("MySQL Error:", e)
+except IndexError:
+    print("Usage: ./script.py <username> <password> <table>")
+except Exception as e:
+    print("An error occurred:", e)
